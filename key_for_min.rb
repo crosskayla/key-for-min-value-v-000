a229a4209e122284b784dc676d76a163893b6652
Kayla Cross <kac288@georgetown.edu>
2018-03-03 18:51:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,8 @@
 def key_for_min_value(name_hash)
   smallest_val_so_far = nil
   name_hash.each do |key, value|
-    if value > smallest_val_so_far
-      smallest_val_so_far = value
+    smallest_val_so_far = value if smallest_val_so_far == nil
+    smallest_val_so_far = value if value > smallest_val_so_far
     end
   end
   return smallest_val_so_far
