ac6c1c312a4f5fa06746d0bb12d5310954a13a23
Kayla Cross <kac288@georgetown.edu>
2018-03-03 18:50:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallest_val_so_far = nil
+  name_hash.each do |key, value|
+    if value > smallest_val_so_far
+      smallest_val_so_far = value
+    end
+  end
+  return smallest_val_so_far
 end
+
+ikea = {:chair => 25, :table => 85, :mattress => 450}
+puts key_for_min_value(ikea)
