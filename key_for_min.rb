a5fc9f1fb6f3ca1b9d0cb94d6c9ecc39ff4992ff
Kayla Cross <kac288@georgetown.edu>
2018-03-03 18:54:09 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,5 +13,5 @@ def key_for_min_value(name_hash)
   return smallest_key
 end
 
-ikea = {:chair => 25, :table => 85, :mattress => 450}
-puts key_for_min_value(ikea)
+veggies = {"apple" => -45, "banana" => -44.5, "carrot" => -44.9}
+key_for_min_value(veggies)
