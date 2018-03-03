accc8e252b12bbf5850ee100e635fe1d899bfa76
Kayla Cross <kac288@georgetown.edu>
2018-03-03 18:53:18 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,12 @@
 def key_for_min_value(name_hash)
   smallest_val_so_far = nil
   name_hash.each do |key, value|
-    smallest_val_so_far = value if smallest_val_so_far == nil
-    smallest_val_so_far = value if value < smallest_val_so_far
+    if smallest_val_so_far == nil || value < smallest_val_so_far
+      smallest_val_so_far = value
+      smallest_key = key
+    end
   end
-  return smallest_val_so_far
+  return smallest_key
 end
 
 ikea = {:chair => 25, :table => 85, :mattress => 450}
